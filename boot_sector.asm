f6c2e89a2b8ac328697dfe4e5b0825a0d3db54b1
Carlos Staszeski <cstaszeski@gmail.com>
2018-06-09 21:42:40 -0400

    Created a simple boot sector for the os

---
+++ boot_sector.asm
@@ -0,0 +1,22 @@
+; This will be the boot sector of my operating system:
+
+; Welcome message:
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10 ; Print the contents of the al register
+mov al, 'e'
+int 0x10
+mov al 'l'
+int 0x10
+int 0x10
+mov al 'o'
+int 0x10
+
+; Infinite loop:
+jmp $
+
+; Fill 510 minus size of previous code 0's
+times 510-($-$$) db 0
+
+; Helps the bios find end of boot sector
+dw 0xaa55
