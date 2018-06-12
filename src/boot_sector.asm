9f5248039a1bbf125ff6b29009a240c6aa0a12b3
CharlosNoTape <cstaz12@gmail.com>
2018-06-12 17:22:25 -0400

    Simplified printing the welcome message by using functions in assembly

--- src/boot_sector.asm
+++ src/boot_sector.asm
@@ -2,23 +2,27 @@
 ; A boot sector that prints a message to the screen using a BIOS routine:
 ;
 
-mov ah, 0x0e	; Scrolling teletype interrupt (tty mode): 
-				; prints al to screen
+[org 0x7c00]	; Tell the assembler that memory addresses start at 0x7c00 (After BIOS memory)
 
-mov al, 'H'	; Load each character into al
-int 0x10	; Call the interrupt
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
-int 0x10
+	mov bx, WELCOME_MESSAGE
+	call print_string
 
-jmp $	; Jump to the current address (Infinite loop)
+	mov bx, GOODBYE_MESSAGE
+	call print_string
 
-times 510-($-$$) db 0	; Fill 510 minus size of previous
-						; code 0's for padding
+	jmp $	; Jump to the current address (Infinite loop)
 
-dw 0xaa55	; Magic number that helps the 
-			; bios find end of boot sector
+%include "print_string.asm"
+
+; Data
+WELCOME_MESSAGE:
+	db 'Booting Charl-OS...',0
+
+GOODBYE_MESSAGE:
+	db 'Goodbye...',0
+
+	times 510-($-$$) db 0	; Fill 510 minus size of previous
+							; code 0's for padding
+
+	dw 0xaa55	; Magic number that helps the 
+				; bios find end of boot sector
