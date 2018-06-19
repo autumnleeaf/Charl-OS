4bba27e4aa93e31c78b94b9fec659e236e357db4
CharlosNoTape <cstaz12@gmail.com>
2018-06-19 19:58:30 -0400

    Add print_nl for formatting strings on a newline

--- src/print_string.asm
+++ src/print_string.asm
@@ -26,4 +26,18 @@ print_string_end:
 	popa
 	ret
 
+
+; Print a newline character
+print_nl:
+	pusha
+
+	mov ah, 0x0e
+	mov al, 0x0a		; newline char
+	int 0x10
+	mov al, 0x0d		; carriage return
+	int 0x10
+
+	popa
+	ret
+
 %endif
