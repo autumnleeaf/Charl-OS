d676464270810c87bfde52c9bedf127dae90ece1
CharlosNoTape <cstaz12@gmail.com>
2018-06-19 19:57:57 -0400

    Add more verbose error messages to disk_load for debugging

--- src/disk_load.asm
+++ src/disk_load.asm
@@ -6,27 +6,39 @@
 ;
 
 disk_load:
-	push dx			; Push dx to the stack so we can check it later
+	pusha
+	push dx				; Push dx to the stack so we can check it later
 
-	mov ah, 0x02	; Specifies the read sector function
+	mov ah, 0x02		; Specifies the read sector function
 
-	mov al, dh		; Read dh number of sectors
-	mov ch, 0x00	; Select cylinder 0
-	mov dh, 0x00	; Select disk 0
-	mov cl, 0x02	; Start from the second sector (after boot sector)
+	mov al, dh			; Read dh number of sectors
+	mov ch, 0x00		; Select cylinder 0
+	mov dh, 0x00		; Select disk 0
+	mov cl, 0x02		; Start from the second sector (after boot sector)
 
-	int 0x13		; Call the BIOS interrupt
+	int 0x13			; Call the BIOS interrupt
 
-	jc disk_error	; If the carry flag is set jump
+	jc disk_error		; If the carry flag is set jump
 
-	pop dx			; Restore dx from the stack
-	cmp al, dh		; See if the number of sectors read == the number expected
-	jne disk_error	; Otherwise print the error
+	pop dx				; Restore dx from the stack
+	cmp al, dh			; See if the number of sectors read == the number expected
+	jne sectors_error	; Otherwise print the error
+	popa
 	ret
 
 disk_error:
 	mov bx, DISK_ERROR_MSG
 	call print_string
+	call print_nl
+	mov dh, ah
+	call print_hex
+	jmp disk_loop
+
+sectors_error:
+	mov bx, SECTORS_ERROR_MSG
+	call print_string
+
+disk_loop:
 	jmp $
 
 
@@ -34,6 +46,10 @@ disk_error:
 DISK_ERROR_MSG:
 	db "Disk read error!",0
 
+SECTORS_ERROR_MSG:
+	db "Incorrect number of sectors read!",0
+
 %include "print_string.asm"
+%include "print_hex.asm"
 
 %endif
