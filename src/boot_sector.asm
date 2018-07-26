bc05edbff9c6b1d28a802967372adc7e4ffd183c
CharlosNoTape <cstaz12@gmail.com>
2018-07-26 13:22:42 -0700

    Load the kernel when booting

--- src/boot_sector.asm
+++ src/boot_sector.asm
@@ -3,12 +3,18 @@
 ;
 
 [org 0x7c00]				; Tell the assembler that memory addresses start at 0x7c00 (After BIOS memory)
+KERNEL_OFFSET equ 0x1000	; The offset where we will store the kernel
+
+	mov [BOOT_DRIVE], dl	; Store the location of the boot drive
+
 	mov bp, 0x9000			; Move the stack to a safe location
 	mov sp, bp
 
 	mov bx, MSG_REAL_MODE
 	call print_string
 	call print_nl
+
+	call load_kernel		; Load the kernel
 
 	call switch_to_pm
 
@@ -19,15 +25,35 @@
 %include "print/print_string_pm.asm"
 %include "pm_switch.asm"
 %include "gdt.asm"
+%include "disk_load.asm"
+
+[bits 16]
+
+load_kernel:
+	mov bx, MSG_LOAD_KERNEL	; Print the loading kernel message
+	call print_string
+	call print_nl
+
+	mov bx, KERNEL_OFFSET	; Set up parameters for disk load to help load the kernel
+	mov dh, 15
+	mov dl, [BOOT_DRIVE]
+	call disk_load
+
+	ret
 
 [bits 32]
 BEGIN_PM:					; Jump here after switch
 	mov ebx, MSG_PROT_MODE
 	call print_string_pm
+
+	call KERNEL_OFFSET		; Jump to the address of the loaded kernel
+
 	jmp $
 
+BOOT_DRIVE db 0
 MSG_REAL_MODE db "Started in 16-bit real mode",0
 MSG_PROT_MODE db "Loaded 32-bit protected mode",0
+MSG_LOAD_KERNEL db "Loading kernel into memory",0
 
 	times 510-($-$$) db 0	; Fill 510 minus size of previous
 							; code 0's for padding
